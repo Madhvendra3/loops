490dcaa62c7d797319274370cfeeccfed6495556
Madhvendra3 <rajawatbunty@gmail.com>
2026-03-20 18:50:20 +0000

    Add files via upload

---
+++ armstrongnum1.py
@@ -0,0 +1,13 @@
+num = int(input("enter a value: "))
+sum = 0
+
+temp = num
+while temp > 0:
+    digit = temp % 10
+    sum += digit ** 3
+    temp //= 10
+
+if num == sum:
+    print("The number is an armstrong number.")
+else:
+    print("The number is not an armstrong number.")       
